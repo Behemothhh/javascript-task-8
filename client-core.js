146e1b31727e1b1c3dbf7fd9cf329675302cb866
Behemothhh <elakov111@gmail.com>
2017-12-08 13:06:57 +0500

    first version

--- client-core.js
+++ client-core.js
@@ -1,11 +1,134 @@
+
 'use strict';
 
 module.exports.execute = execute;
 module.exports.isStar = true;
 
+const got = require('got');
+const ArgumentParser = require('argparse').ArgumentParser;
+const chalk = require('chalk');
+const parser = new ArgumentParser();
+const url = 'http://localhost/messages/';
+
+parser.addArgument('command', {
+    help: 'command name',
+    choices: ['list', 'send', 'delete', 'edit']
+});
+
+parser.addArgument('--from', { help: 'message sender' });
+parser.addArgument('--to', { help: 'message recipient' });
+parser.addArgument('--text', { help: 'message text' });
+parser.addArgument('--id', { help: 'message id' });
+parser.addArgument('-v', { help: 'show message details', action: 'storeTrue' });
+
 function execute() {
     // Внутри этой функции нужно получить и обработать аргументы командной строки
-    const args = process.argv;
+    const args = parser.parseArgs();
+    console.info(args);
+    switch (args.command) {
+        case 'list':
+            return listFunction(args);
+        case 'send':
+            return sendFunction(args);
+        case 'delete':
+            return deleteFunction(args);
+        case 'edit':
+            return editFunction(args);
+        default:
+            break;
+    }
 
-    return Promise.resolve('Это строка будет выведена на консоль');
+}
+
+function listFunction(args) {
+    return got(url, {
+        json: true,
+        port: 8080,
+        query: createQuery(args)
+    })
+        .then(response => {
+            console.info(response.body);
+
+            return response.body
+                .reduce((answer, element) => answer.concat(prepareText(element)), [])
+                .join('\n\n');
+        });
+}
+
+function sendFunction(args) {
+    return got(url, {
+        method: 'POST',
+        port: 8080,
+        query: createQuery(args),
+        json: true,
+        body: { text: args.text }
+    })
+        .then(response => {
+            console.info(response.body);
+
+            return prepareText(response.body);
+        });
+}
+
+function deleteFunction(args) {
+    return got(url + args.id, {
+        method: 'DELETE',
+        port: 8080,
+        json: true
+    })
+        .then(response => {
+            console.info(response.body);
+            if (response.body.status === 'ok') {
+                return 'DELTED';
+            }
+        });
+}
+
+function editFunction(args) {
+    return got(url + args.id, {
+        method: 'PATCH',
+        port: 8080,
+        json: true,
+        body: { text: args.text }
+    })
+        .then(response => {
+            console.info(response.body);
+
+            return prepareText(response.body);
+        });
+}
+
+function prepareText(data) {
+    let tempAnswer = '';
+    if (Number.isInteger(parseInt(data[0]))) {
+        tempAnswer += (chalk.hex('#FF0')('ID') + ': ' + data[0] + '\n');
+        data = data[1];
+    }
+    if (data.from) {
+        tempAnswer += (chalk.hex('#F00')('FROM') + ': ' + data.from + '\n');
+    }
+    if (data.to) {
+        tempAnswer += (chalk.hex('#F00')('TO') + ': ' + data.to + '\n');
+    }
+    if (data.text) {
+        const editedText = data.edited ? chalk.hex('#777')('(edited)') : '';
+        tempAnswer += (chalk.hex('#0F0')('TEXT') + ': ' + data.text + editedText);
+    }
+
+    return tempAnswer;
+}
+
+function createQuery(args) {
+    const tempQuery = {};
+    if (args.from) {
+        tempQuery.from = args.from;
+    }
+    if (args.to) {
+        tempQuery.to = args.to;
+    }
+    if (args.v) {
+        tempQuery.id = args.v;
+    }
+
+    return tempQuery;
 }
--- server-core.js
+++ server-core.js
@@ -1,12 +1,75 @@
 'use strict';
 
-const http = require('http');
+const server = require('express')();
+const bodyParser = require('body-parser');
 
-const server = http.createServer();
+const jsonParser = bodyParser.json();
 
-server.on('request', (req, res) => {
-    // Тут нужно обработать запрос
-    res.end();
-});
+const messages = {};
+
+let idCounter = 0;
+
+server.route('/messages')
+    .get(sendMessages)
+    .post(jsonParser, getMessage);
+
+server.route('/messages/:id')
+    .delete(deleteMessage)
+    .patch(jsonParser, editMessage);
+
+
+function editMessage({ body, params, query }, res) {
+    const id = params.id;
+    if (!body.text || !messages[id]) {
+        return res.sendStatus(400);
+    }
+    messages[id].text = body.text;
+    messages[id].edited = true;
+
+    res.json(query.id ? [id, messages[id]] : messages[id]);
+}
+
+function deleteMessage({ params }, res) {
+    delete messages[params.id];
+
+    return res.json({ status: 'ok' });
+}
+
+function sendMessages({ query }, res) {
+    res.json(Object.entries(messages)
+        .filter(message => {
+            const isToEqual = !query.to || low(query.to) === low(message[1].to);
+            const isFromEqual = !query.from || low(query.from) === low(message[1].from);
+
+            return isToEqual && isFromEqual;
+        })
+        .map(message => query.id ? message : message[1]));
+}
+
+function getMessage({ body, query }, res) {
+    if (!body.text) {
+        return res.sendStatus(400);
+    }
+    const newMessage = { text: body.text };
+    if (query.from) {
+        newMessage.from = query.from;
+    }
+    if (query.to) {
+        newMessage.to = query.to;
+    }
+    const id = idCounter++;
+
+    messages[id] = newMessage;
+
+    res.json(query.id ? [id, newMessage] : newMessage);
+}
+
+function low(text) {
+    if (text === undefined) {
+        return false;
+    }
+
+    return text.toLowerCase();
+}
 
 module.exports = server;
